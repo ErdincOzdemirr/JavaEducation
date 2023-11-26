83a3217ccfdf3deff1e7104c02911d9e436dff66
Erdinc <127399545+ErdincOzdemirr@users.noreply.github.com>
2023-11-26 14:38:59 +0300

    Methods
    Methods Basic

--- Java_006_Methods/src/main/java/com/erdincozdemir/_001_Metot.java
+++ Java_006_Methods/src/main/java/com/erdincozdemir/_001_Metot.java
@@ -1,4 +1,28 @@
 package com.erdincozdemir;
-
 public class _001_Metot {
+
+    public static void cizgiCiz() { System.out.println("---------------");}
+
+    public static void topla(int a, int b){
+       int toplama = a + b;
+        System.out.println("toplam = " + toplama);
+    }
+
+    public static void kucukBul(int x, int y) {
+        if (x>y){
+            System.out.println("x, y'den büyük");
+        } else if(x<y){
+            System.out.println(("x,y'den küçük"));
+        } else{
+            System.out.println("x = y");
+        }
+    }
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+            cizgiCiz();
+        }
+        topla(1,2);
+        kucukBul(8,12);
+    }
 }
