b10d4a91195b2f81865e64319431cc60edcf882e
Erdinc <127399545+ErdincOzdemirr@users.noreply.github.com>
2023-12-18 00:29:22 +0300

    Hibernate

--- Java_026_JDBC/src/main/java/com/erdincozdemir/AppMain.java
+++ Java_026_JDBC/src/main/java/com/erdincozdemir/AppMain.java
@@ -2,6 +2,6 @@ package com.erdincozdemir;
 
 public class AppMain {
     public static void main(String[] args) {
-
+        System.out.println("Erdinç");
     }
 }
--- Java_026_JDBC/src/main/java/com/erdincozdemir/select/MySqlConnection.java
+++ Java_026_JDBC/src/main/java/com/erdincozdemir/select/MySqlConnection.java
@@ -1,4 +1,42 @@
 package com.erdincozdemir.select;
 
+import java.sql.*;
+
 public class MySqlConnection {
+    public static void main(String[] args) throws SQLException, RuntimeException {
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try
+
+        //Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/myrealdb",
+                "root",
+                "He181215"
+
+        );
+
+        statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM person");
+
+        while (resultSet.next()) {
+            System.out.println(resultSet.getInt("id") + " " + resultSet.getString("first_name") + " " + resultSet.getString("last_name"));
+        }
+
+
+        finally{
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+
+
+    }
+
+
 }
+
+
