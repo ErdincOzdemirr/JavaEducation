c1f053d906e976c6fe60756b71a92ce3cdb529a6
Erdinc <127399545+ErdincOzdemirr@users.noreply.github.com>
2023-12-20 23:16:43 +0300

    StreamApi

--- Java_021_StreamApi/src/main/java/com/erdincozdemir/AppMain.java
+++ Java_021_StreamApi/src/main/java/com/erdincozdemir/AppMain.java
@@ -1,9 +1,18 @@
 package com.erdincozdemir;
 
+import java.util.stream.Stream;
+
 public class AppMain {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Stream <String> boskayit = Stream.empty();
+        System.out.println("boskayit bilgisi: "+boskayit);
+
+        Stream <String> tekKayit = Stream.of("Erdinç");
+        System.out.println("Tek Kayit Bilgisi : "+tekKayit);
+
+        Stream <String> cokluKayit = Stream.of("Erdinç", "Hava", "Ezgi", "Hacer", "ismail");
+        System.out.println("Coklu Kayit Bilgisi : "+cokluKayit);
 
     }
 }
