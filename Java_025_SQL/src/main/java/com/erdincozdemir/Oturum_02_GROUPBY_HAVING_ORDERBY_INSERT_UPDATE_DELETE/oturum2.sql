7214ac0f59a52530097b0e8a0290c7de06aadfc7
Erdinc <127399545+ErdincOzdemirr@users.noreply.github.com>
2024-01-02 22:29:37 +0300

    sql - GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE

--- Java_025_SQL/src/main/java/com/erdincozdemir/Oturum_02_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/oturum2.sql
+++ Java_025_SQL/src/main/java/com/erdincozdemir/Oturum_02_GROUPBY_HAVING_ORDERBY_INSERT_UPDATE_DELETE/oturum2.sql
@@ -1,13 +1,4 @@
--- VERITABANI OLUSTURULDU
-CREATE SCHEMA `mydemodb` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin ;
-
-CREATE TABLE `mydemodb`.`customers` (
-                                        `customer_id` INT NOT NULL AUTO_INCREMENT,
-                                        `first_name` VARCHAR(45) NOT NULL,
-                                        `last_name` VARCHAR(60) NOT NULL,
-                                        `phone` VARCHAR(35) NULL,
-                                        `country` VARCHAR(50) NULL,
-                                        PRIMARY KEY (`customer_id`));
+SELECT * FROM mydemodb.customers;
 
 INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Ali Yağız', 'Tar', '333', 'TR');
 INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`) VALUES ('Mehmet Ali', 'Kaya', '333', 'CA');
@@ -27,36 +18,28 @@ INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country
 
 SELECT * FROM mydemodb.customers;
 
-
 -- Tüm kayıtlar
-SELECT * FROM customers;
-
--- Ülkesi TR olan kayıtlar
-SELECT * FROM customers WHERE country = 'TR';
-
--- Bütün ülkeler
-SELECT country FROM  customers;
-
-
--- Bütün ülkeler tekil
-SELECT DISTINCT country FROM  customers;
-
-
--- Ülkesi CA olmayanlar
-SELECT * FROM customers WHERE NOT country = 'CA';
-
+SELECT * FROM mydemodb.customers;
 
 -- Ülkesi TR olmayanlar
-SELECT * FROM customers WHERE country != 'TR';
+SELECT * FROM mydemodb.customers WHERE country != 'TR';
 
+-- Bütün ülkeler
+SELECT customers.country FROM customers;
+
+-- Bütün ülkeler tekil
+SELECT DISTINCT customers.country FROM customers;
+
+-- Ülkesi CA olmayanlar
+SELECT * FROM customers WHERE NOT country='CA';
+
+-- Ülkesi TR olmayanlar
+SELECT * FROM customers WHERE NOT country ='TR';
 
 ALTER TABLE `mydemodb`.`customers`
-    ADD COLUMN `age` INT NULL DEFAULT 0 AFTER `country`;
+    ADD COLUMN `age` INT NOT NULL DEFAULT 0 AFTER `country`;
 
--- Eklenen kolondan sonra tablonun son durumu
-SELECT * FROM customers;
-
-
+SELECT * FROM mydemodb.customers;
 
 UPDATE mydemodb.customers t
 SET t.age = 23
@@ -120,26 +103,22 @@ WHERE t.customer_id = 9;
 
 
 -- Yasi 22'den büyük olanlar
-SELECT * FROM customers  WHERE  age > 22;
+SELECT * FROM customers WHERE age>22;
 
 -- Hangi ülkeden toplam kaç tane var?
 SELECT COUNT(DISTINCT country) FROM customers;
 
 -- Alias (lakap, takma isim, takma ad)
-SELECT customer_id AS cus_id FROM customers;
-
-
--- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
-SELECT customer_id AS cus_id, first_name, last_name  FROM customers;
+SELECT customers.customer_id AS cus_id FROM customers;
 
 -- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
-SELECT customer_id AS cus_id, first_name AS f_name, last_name as l_name  FROM customers;
+SELECT customer_id AS cus_id, first_name, last_name FROM customers;
 
 -- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
-SELECT customer_id cus_id, first_name f_name, last_name l_name  FROM customers;
+SELECT  customers.customer_id AS cus_id, customers.first_name As f_name, customers.last_name AS l_name FROM customers;
 
 -- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
-SELECT customer_id id, first_name first, last_name last  FROM customers;
+SELECT  customers.customer_id id, customers.first_name f_name, customers.last_name l_name FROM customers;
 
 -- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
 SELECT customer_id id, first_name f, last_name l  FROM customers c;
@@ -147,31 +126,26 @@ SELECT customer_id id, first_name f, last_name l  FROM customers c;
 -- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
 SELECT c.customer_id, c.first_name, c.last_name  FROM customers c;
 
--- Alias (lakap, takma isim, takma ad) ve diğer kolonlardan birkaçı
 SELECT c.customer_id AS id,
        c.first_name AS f_name,
        c.last_name AS l_name
-FROM customers AS c;
+FROM customers c;
 
 -- CONCAT birleştirme
-SELECT CONCAT(first_name, ' ' ,last_name) FROM customers;
+SELECT CONCAT(customers.first_name, ' ' ,customers.last_name ) FROM customers;
 
 -- CONCAT birleştirme
-SELECT CONCAT(first_name, ' ' ,last_name) AS full_name FROM customers;
+SELECT CONCAT(customers.first_name, ' ' ,customers.last_name ) AS FullName FROM customers;
 
 -- Bir kayıt ekleme
-INSERT INTO customers (first_name, last_name, phone,  country , age)
-VALUES ('Fatih'   ,'Özcan'   ,'1453',  'TR'    , 21);
+INSERT INTO customers(first_name, last_name, phone, country, age) VALUES ('Hava','Sevgili','1812','06','31');
 
-
-SELECT * FROM customers WHERE first_name = "Fatih";
+SELECT * FROM customers WHERE first_name = 'Hava';
 
 -- Birden fazla kayıt ekleme
-INSERT INTO customers (first_name, last_name, phone,  country , age)
-VALUES ('Senem','Hanım','195','CA',21),
-       ('Servet','Demir','143','TR',24),
-       ('Ali Ensar','Ayabakan','455','US', 33);
-
+INSERT INTO customers(first_name, last_name, phone, country, age) VALUES ('Hacer','Sevgili','1812','06','49'),
+                                                                         ('Ezgi','Sevgili','1812','06','27'),
+                                                                         ('İsmail','Sevgili','1812','06','59');
 SELECT * FROM customers WHERE customer_id > 16;
 
 -- SAKIN BÖYLE YAPMAYIN!!!! WHERE şart koymak zorundasınız!
@@ -181,92 +155,7 @@ SET age = 20;
 */
 
 -- CusId 19 olan müşterinin yaşını 20 olarak güncelle.
-UPDATE customers
-SET age = 20
-WHERE customer_id = 19;
+UPDATE customers SET customers.age = 20 WHERE customer_id=19;
 
 SELECT * FROM customers WHERE customer_id = 19;
 
-
--- SAKIN SAKIN BÖYLE YAPMAYIN! Silmede ŞART KESİNLİKLE OLMALI.
--- DELETE FROM customers;
-
-SELECT * FROM customers WHERE  customer_id = 2;
-
--- SADECE idsi 2 olan kaydı sileceğiz
-DELETE FROM customers
-WHERE customer_id = 2;
-
--- Bütün kayıtlar ama 2. kayıt silindi yok.
-SELECT * FROM customers;
-
--- Tablonun içini ilk oluşturulma anına alır. Sayaçlarını da.
--- TRUNCATE TABLE customers;
-
-INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`, `age`)
-VALUES ('Mehmet Ali', 'Kaya', '9999', 'CA', 26);
-
-
--- A'dan Z'ye isim kolonuna göre sırala -- VARSAYILAN A --> Z
-SELECT * FROM customers
-ORDER BY first_name;
--- ORDER BY first_name ASC;
-
--- Z'den A'ya isim kolonuna göre sırala
-SELECT * FROM customers
-ORDER BY first_name DESC;
-
--- A'dan Z'ye yaşa göre sırala en küçükten  en büyüğe
-SELECT * FROM customers
-ORDER BY age ASC;
-
--- Z'den A'ya yaşa göre sırala en büyükten en küçüğe
-SELECT * FROM customers
-ORDER BY age DESC;
-
-
--- Z'den A'ya önce ülkelere göre sonra da yaşa göre sırala en büyükten en küçüğe
-SELECT * FROM customers
-ORDER BY  country DESC, age DESC;
-
--- Ülke isimlerini al ve kişi sayısına göre sırala
-SELECT country, COUNT(*) AS numbers FROM customers
-GROUP BY country;
-
-
-INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`, `age`)
-VALUES ('Fatih', 'Özcan', '1999', 'DE', 25);
-
-INSERT INTO `mydemodb`.`customers` (`first_name`, `last_name`, `phone`, `country`, `age`)
-VALUES ('Fatih', 'Bey', '2222', 'DE', 42);
-
--- Ülke isimlerini al ve kişi sayısına göre sırala
-SELECT country, first_name, COUNT(*) AS numbers FROM customers
-GROUP BY  country, first_name;
-
--- Ülke isimlerini al ve kişi sayısına göre sırala - gruba şart ekle
-SELECT country, first_name, COUNT(*) AS numbers FROM customers
-GROUP BY  country, first_name
-HAVING country ='DE';
-
--- Ülke isimlerini al ve kişi sayısına göre sırala - gruba şart ekle
-SELECT country, first_name, COUNT(*) AS numbers FROM customers
-WHERE age > 20 AND country = 'DE'
-GROUP BY  country, first_name;
-
--- Ülke isimlerini al ve kişi sayısına göre sırala - gruba şart ekle
-SELECT country, first_name, COUNT(*) AS numbers FROM customers
--- WHERE age > 22
-GROUP BY  country, first_name
-HAVING country ='DE';
-
-
--- Ülke isimlerini al ve kişi sayısına göre sırala - gruba şartı Almanya olmayanlar
-SELECT country, first_name, COUNT(*) AS numbers FROM customers
-GROUP BY  country, first_name
-HAVING NOT country ='DE';
-
--- Ülke isimlerini al ve kişi sayısına göre sırala - gruba şartı Almanya olmayanlar
-SELECT country, first_name, COUNT(*) AS numbers FROM customers
-GROUP BY  country, first_name
-HAVING country !='DE';
