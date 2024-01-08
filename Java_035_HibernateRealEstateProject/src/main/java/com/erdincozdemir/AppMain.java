e25f55172e03253ccc857b16e6989acda9123b03
Erdinc Ozdemir <127399545+ErdincOzdemirr@users.noreply.github.com>
2024-01-09 00:23:08 +0300

    Spring Boot

--- Java_035_HibernateRealEstateProject/src/main/java/com/erdincozdemir/AppMain.java
+++ Java_035_HibernateRealEstateProject/src/main/java/com/erdincozdemir/AppMain.java
@@ -2,6 +2,6 @@ package com.erdincozdemir;
 
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello world! Ben Erdinç");
+        System.out.println("Hello world! Erdinç");
     }
 }
