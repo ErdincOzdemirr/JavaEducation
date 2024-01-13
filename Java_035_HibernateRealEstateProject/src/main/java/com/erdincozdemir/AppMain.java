9afd443863c4b46ee3e4101e3ac2b55772b88386
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-01-13 16:03:11 +0300

    Spring Boot Start

--- Java_035_HibernateRealEstateProject/src/main/java/com/erdincozdemir/AppMain.java
+++ Java_035_HibernateRealEstateProject/src/main/java/com/erdincozdemir/AppMain.java
@@ -2,6 +2,6 @@ package com.erdincozdemir;
 
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! This is Spring Boot");
     }
 }
