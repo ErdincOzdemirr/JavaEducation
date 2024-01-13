d7f686f99a5c40fd968b40040f176f0476d387e5
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-01-14 01:18:14 +0300

    Spring Boot

--- Java_036_SpringHelloProject/src/main/java/com/erdincozdemir/contreller/StudentController.java
+++ Java_036_SpringHelloProject/src/main/java/com/erdincozdemir/contreller/StudentController.java
@@ -2,17 +2,23 @@ package com.erdincozdemir.contreller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //@Controller // Web sayfaları //eski projelerde görürsün.
-@RestController //RESTful // Web projeniz farklı konumdaysa json veri formatında çıktı alınır. 
+@RestController //RESTful // Web projeniz farklı konumdaysa json veri formatında çıktı alınır.
 public class StudentController {
 
-     //http://localhost:8082
+     //http://localhost:8090
+     //http://127.0.0.1:8090
      @GetMapping
      public String getHello() {
           return "Spring Boot Hello World! This sentence was written by Erdinc! ";
      }
 
+     @DeleteMapping
+     public String getHello2() {
+          return "Delete Boot Erdinc";
+     }
 }
