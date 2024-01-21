9dba8362c22829dd95a79db101450e69f9834061
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-01-21 20:13:19 +0300

    spring boot

--- Java_039_SpringBootException/src/main/java/com/erdincozdemir/exception/ResourceNotFoundException.java
+++ Java_039_SpringBootException/src/main/java/com/erdincozdemir/exception/ResourceNotFoundException.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ResourceNotFoundException extends Exception {
     public ResourceNotFoundException(String message) {
         super(message);
-        
+
     }
 }
