86d16407abf38065672c388b21b0286408d80da4
Erdinc Ozdemir <erdincozdemir1812@gmail.com>
2024-01-27 17:55:45 +0300

    MicroServicesDemo

---
+++ Java_042_MicroserviceDemo/src/test/java/com/erdincozdemir/Java042MicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erdincozdemir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java042MicroserviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
